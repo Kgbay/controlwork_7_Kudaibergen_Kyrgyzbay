cace6719a7a57d99b75be181f016443d858c3a00
Kudaibergen Kyrgyzbay <kyrgyzbay.kudaibergen@gmail.com>
2023-02-25 17:50:25 +0600

    Добавил форму для редактирвония данными

--- source/guest_book_app/urls.py
+++ source/guest_book_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
 from .views.base import index_view
-from .views.guests import guest_add_view
+from .views.guests import guest_add_view, guest_update_view, guest_remove_view
 
 urlpatterns = [
-    path("", index_view, name='index_view')
+    path("", index_view, name='index_view'),
+    path("guest/add/", guest_add_view, name="guest_add"),
+    path("guest/<int:pk>/update/", guest_update_view, name="guest_edit"),
+    path("guest/<int:pk>/remove/", guest_remove_view, name="guest_remove")
 ]
--- source/guest_book_app/views/guests.py
+++ source/guest_book_app/views/guests.py
@@ -1,5 +1,33 @@
 from django.core.handlers.wsgi import WSGIRequest
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+
+from guest_book_app.models import Guest
+
 
 def guest_add_view(request: WSGIRequest):
-    return render(request, 'guest.html')
+    if request.method == "GET":
+        guests = Guest.objects.all()
+        context = {'guests': guests}
+        return render(request, 'guest_create.html', context=context)
+    guest_data = {
+        'author': request.POST.get('author'),
+        'mail': request.POST.get('mail'),
+        'text': request.POST.get('text')
+    }
+    guest = Guest.objects.create(**guest_data)
+    return redirect('index_view')
+
+def guest_update_view(request, pk):
+    guest = get_object_or_404(Guest, pk=pk)
+    if request.method == 'POST':
+        guest.author = request.POST.get('author')
+        guest.mail = request.POST.get('mail')
+        guest.text = request.POST.get('text')
+        guest.save()
+        return redirect('index_view')
+    return render(request, 'guest_update.html', context={
+        'guest': guest
+    })
+
+
+

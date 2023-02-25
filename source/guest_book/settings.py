e16a2984d87be933075aa0245c37409220b6ae46
Kudaibergen Kyrgyzbay <kyrgyzbay.kudaibergen@gmail.com>
2023-02-25 11:45:41 +0600

    Провел первую миграцию после присвоение БД как postgres и определении всех совокупностей

--- source/guest_book/settings.py
+++ source/guest_book/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'guest_book.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "guest_book_db",
+        "USER": "postgres",
+        "PASSWORD": "kuka2305",
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 

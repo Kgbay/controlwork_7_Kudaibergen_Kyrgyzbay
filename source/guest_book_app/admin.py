56837de6935392a2356fc9a02d6f696e385545df
Kudaibergen Kyrgyzbay <kyrgyzbay.kudaibergen@gmail.com>
2023-02-25 13:17:11 +0600

    Зарегистрировал модель в админ панели

--- source/guest_book_app/admin.py
+++ source/guest_book_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Guest
+
+
 # Register your models here.
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'mail')
+    list_filter = ('id', 'author', 'created_at')
+    search_fields = ('author', 'text')
+    fields = ('author', 'mail', 'text', 'status')
+    readonly_fields = ('id', 'created_at', 'updated_at')
+
+
+admin.site.register(Guest, GuestAdmin)
